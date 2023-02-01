7b1187c317b1ae6aed16da2cbc1a43658ec5b453
Stephen Walton <info@stevethedeveloper.com>
2023-01-31 16:13:03 -0800

    session

--- ML Test/ViewController.swift
+++ ML Test/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +27,24 @@ class ViewController: UIViewController {
         
         // Start the capture session
         captureSession.startRunning()
+        
+        // create a preview layer for the captureSession
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        // add the previewLayer as a sublayer on our main view
+        view.layer.addSublayer(previewLayer)
+        
+        // link view's frame with previewLayer's frame
+        previewLayer.frame = view.frame
+        
+        // create the outputData
+        let outputData = AVCaptureVideoDataOutput()
+        
+        // Sets the sample buffer delegate and the queue for invoking callbacks
+        // first param is an object conforming to the AVCaptureVideoDataOutputSampleBufferDelegate protocol that will receive sample buffers after they are captured.
+        // We must use a serial dispatch queue to guarantee that the video frames will be delivered in order
+        outputData.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoFrames.queue"))
+        captureSession.addOutput(outputData)
     }
 
 
