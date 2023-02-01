ea486ca9478f4da72570a7752df6ec0426bac4b7
Stephen Walton <info@stevethedeveloper.com>
2023-01-31 16:01:55 -0800

    init

--- ML Test/ViewController.swift
+++ ML Test/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create a new capture session using the AVCaptureSession constructor
+        let captureSession = AVCaptureSession()
+        
+        // Create a capture device using AVCaptureDevice
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        
+        // Create an input for the capture device
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+        // Set the input to the capture session
+        captureSession.addInput(input)
+        
+        // Start the capture session
+        captureSession.startRunning()
     }
 
 
